8ae45d3eb54d0c56abe408a8c42adb7df0e29bdc
Gene Ross <gene@Genes-iMac.local>
2015-06-08 10:52:22 -0400

    added index.php and assets/

---
+++ assets/js/project.name.js
@@ -0,0 +1,18 @@
+var ProjectName = (function($) {
+
+	var init = function() {
+
+		// Set up any all 
+		// default libraries here
+
+	};
+	
+	return {
+		init: init
+	};
+
+}(jQuery));
+
+$(function() {
+	ProjectName.init();
+});
---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+	
+		<meta charset="utf-8">
+		<title>Project Name</title>
+		
+		<!-- Set Keywords & Description -->
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		
+		<!-- Set Viewport Options -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"/>
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		
+		<!-- Include Fonts -->	
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600italic,600,700,700italic" rel="stylesheet" type="text/css">		
+		
+		<!-- Include Stylesheets -->
+		<link rel="stylesheet" href="assets/css/main.css" />
+
+		<!-- Include Libraries -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="assets/js/lib/waypoints.min.js"></script>
+		<script src="assets/js/lib/modernizr-2.6.2.min.js"></script>
+		<script src="assets/js/lib/respond.min.js"></script>
+
+		<!-- Include Scripts -->
+		<script src="assets/js/project.name.js"></script>
+
+	</head>
+	
+	<body>
+	
+		<header>
+		
+			<nav>
+
+			</nav><!-- end nav -->
+		
+		</header><!-- end header -->
+		
+		<section class="home">
+
+		</section><!-- end .home -->
+
+		<footer>
+
+		</footer><!-- end footer -->
+
+	</body>
+
+</html>
